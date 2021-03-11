b2ba6a03763372480f6feae23fb98180f87341d6
Aymene <aymene01@hotmail.fr>
2021-03-11 09:14:39 +0100

    middlewares

--- Back-end/src/server.js
+++ Back-end/src/server.js
@@ -4,8 +4,11 @@ import cors from 'cors'
 
 const server = () => {
     const app = express()
-
     
+    app.use(cors())
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.json())
+
     const port = process.env.PORT || 4000
     app.listen(port, () => console.log(`server is running on port ${port}`))
 }
